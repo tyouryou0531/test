6d35d5e36711f78c8b6917e635ff108b1f2de895
tyouryou0531 <43364517+tyouryou0531@users.noreply.github.com>
2018-10-01 18:12:56 +0900

    0.0.1

--- friendlyhello/app.py
+++ friendlyhello/app.py
@@ -13,7 +13,7 @@ def hello():
     try:
         visits = redis.incr("counter")
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits = "<i>cannot connect to Redis, counter disabled!</i>"
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
